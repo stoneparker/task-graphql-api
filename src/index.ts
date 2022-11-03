a1baf4a4bc8a0cdf7a25e946be577704b919b069
vitoria lopes <heyvitoria.lopes@gmail.com>
2022-11-03 20:50:15 -0300

    new: remove hello world

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,12 @@ import path from 'node:path';
 
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
-import { HelloWorldResolver } from './resolvers/hello-world';
 import { TasksResolver } from './resolvers/tasks';
 import { UsersResolver } from './resolvers/users';
 
 async function bootstrap() {
   const schema = await buildSchema({
-    resolvers: [HelloWorldResolver, TasksResolver, UsersResolver],
+    resolvers: [TasksResolver, UsersResolver],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
   });
 
--- src/resolvers/hello-world.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from 'type-graphql';
-
-@Resolver(() => String)
-export class HelloWorldResolver {
-  @Query(() => String)
-  async helloWorld() {
-    return 'Hello World :)'
-  }
-}
